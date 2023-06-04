8c424440cbd41cf85f6331ac581a581892176442
Gustavo <gustavoneripeixoto@gmail.com>
2023-06-04 10:52:48 -0700

    Product Item class added

--- 3 - OOP/midterm/assets/scripts/app.js
+++ 3 - OOP/midterm/assets/scripts/app.js
@@ -1,11 +1,21 @@
 // Classes
- 
-class Product{
-  constructor(id, title, price, description, image){
-    this.id = id,
-    this.title = title,
-    this.price = price,
-    this.description = description;
-    this.image = image;
-  }
-}
+
+// Step 1 - Product class
+const product = {
+  id: 1,
+  title: "...",
+  price: 0,
+  description: "...",
+  image: "...",
+};
+
+// Step 2 - Product Item
+const productItem = {
+  product: {}, // instance of Product class
+  addToCart() {
+    // just console.log the product for now
+  },
+  render() {
+    // return the product element with the product data (<li>)
+  },
+};
